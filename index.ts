076a8b0b96d7700e359bdb489d7ba0b882a7dd3d
Siddharth Arora <siddhartharora@Siddharths-Laptop.local>
2024-06-20 13:24:58 +0530

    Added products interface and pricing rule info

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+// Define the Product interface
+interface Product {
+  name: string;
+  price: number;
+}
+
+// Define the product catalogue as an object
+const products: { [sku: string]: Product } = {
+  ipd: { name: 'Super iPad', price: 549.99 },
+  mbp: { name: 'MacBook Pro', price: 1399.99 },
+  atv: { name: 'Apple TV', price: 109.50 },
+  vga: { name: 'VGA adapter', price: 30.00 }
+};
+
+// Define the PricingRule interface
+interface PricingRule {
+  apply: (items: string[]) => number;
+}
+
